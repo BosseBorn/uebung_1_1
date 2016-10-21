800c25f8f97d712f8a72e627ca5489aa4379b581
14951 <14951@A101-06.nordakademie.intern>
2016-10-21 11:17:50 +0200

    PQ Formel

--- src/HalloWelt.java
+++ src/HalloWelt.java
@@ -4,6 +4,8 @@ public class HalloWelt {
 	public static void main(String[] args) {
 		System.out.println("Hallo Welt!");
 
+		System.out.print("Textzeile");
+		System.out.println("Hoppala!");
 	}
 
 }
